909aad09067d48e8f97784511e5fe70205d5677f
JoaoLeonello <jfelipe.pl@gmail.com>
2025-01-02 08:31:12 -0300

    feat: adds user to organization feature controller add

--- server/modules/organizations/controllers/OrganizationController.js
+++ server/modules/organizations/controllers/OrganizationController.js
@@ -16,6 +16,21 @@ class OrganizationController {
         return res.status(400).json({ error: error.message });
       }
     }
+
+    async addUserToOrganization(req, res) {
+      try {
+        const { orgId } = req.params;
+        const { user_id, role } = req.body;
+        const userAdded = await this.organizationService.addUserToOrganization({
+          user_id,
+          org_id: orgId,
+          role
+      });
+      return res.status(201).json({ data: userAdded });
+      } catch (error) {
+        return res.status(400).json({ error: error.message });
+      }
+    }
   }
   
   module.exports = OrganizationController;
